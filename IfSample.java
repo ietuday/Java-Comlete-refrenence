44963f72ef7475691ff1acfc2ed02ecce11bdbc6
Udayaditya Singh <udayaditya.singh@gmail.com>
2019-12-02 15:09:46 +0530

    fisrt

---
+++ IfSample.java
@@ -0,0 +1,18 @@
+class IfSample{
+    public static void main(String args[]) {
+        int x,y;
+
+        x = 10;
+        y = 20;
+
+        if(x < y) System.out.println("X is less than y");
+
+        x = x*2;
+        if(x == y) System.out.println("X now equal to y");
+
+        x = x*2;
+        if(x > y) System.out.println("x now greater than y");
+
+        if( x == y) System.out.println("You won't see this");
+    }
+}
